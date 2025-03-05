25c7e3e8d89411f369015f97114bfc9814c5cbc7
Velleine Alexis <alvelleine@gmail.com>
2025-03-05 22:42:25 +0100

    ajout du check du service de notif

--- src/cron/tasks.ts
+++ src/cron/tasks.ts
@@ -12,6 +12,18 @@ cron.schedule("*/15 * * * * *", () => {
   });
 });
 
+// Monitoring du Service de Notification
+// S'exécute toutes les 15 secondes
+// Vérifie l'endpoint /health qui renvoie le statut du service
+cron.schedule("*/15 * * * * *", () => {
+  fetch("http://localhost:3000/health").then((response) => {
+    // Si le service répond avec un statut 2xx
+    if (response.ok) sendInfo("Notification Service", "Online");
+    // Si le service ne répond pas ou renvoie une erreur
+    else sendWarn("Notification Service", "Offline");
+  });
+});
+
 // export const notOnline: cron.ScheduledTask = cron.schedule(
 //   "* * * * * *",
 //   () => {
--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/off", (req, res) => {
-  startCronJob(notOnline);
-  res.send("Task notOnline has been executed");
-});
-
-app.get("/on", (req, res) => {
-  stopCronJob(notOnline);
-  res.send("Task notOnline has been stopped");
-});
-
 app.get("/", (req, res) => {
   sendSend(WentOffline);
   res.send("Hello World!");
